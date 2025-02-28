bdc1069ed31b65a25fcfb88140ba67109c37e0db
Mateus Paiva <mateusopaiva@gmail.com>
2025-02-28 15:40:07 -0300

    Trabalhando com as Transactions na prática no SQL e no código

--- scripts/01-transactions.sql
+++ scripts/01-transactions.sql
@@ -1,18 +1,20 @@
 \c live026
 
+--DROP TABLE bank_accounts;
 --CREATE TABLE bank_accounts (
 --  id SERIAL PRIMARY KEY,
 --  user_id INT,
 --  balance NUMERIC(10,2) CHECK (balance >= 0)
 --);
 
---INSERT INTO bank_accounts(user_id, balance)
---  VALUES(1,500), (2, 100);
+--INSERT INTO bank_accounts(user_id, balance) VALUES(1,500), (2, 100);
 
 --UPDATE bank_accounts SET balance = balance - 50 WHERE user_id = 1;
 --UPDATE bank_accounts SET balance = balance + 50 WHERE user_id = 2;
 
+BEGIN;
 UPDATE bank_accounts SET balance = balance + 200 WHERE user_id = 1;
 UPDATE bank_accounts SET balance = balance - 200 WHERE user_id = 2;
+COMMIT;
 
 SELECT user_id, balance FROM bank_accounts;
